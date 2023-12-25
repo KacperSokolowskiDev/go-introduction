81a87547cd42675268dceaf2e7404269a4469739
Kacper <kacperbxl@gmail.com>
2023-12-25 05:04:47 +0100

    adding

--- go-essentials/go-practice1/profit_calculator.go
+++ go-essentials/go-practice1/profit_calculator.go
@@ -1,25 +1,50 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		//panic(err)
+		return
+	}
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	taxRate, err := getUserInput("Tax Rate: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 	
-	fmt.Printf("ebt: %.2f, profit: %.2f, ratio: %.2f\n", ebt, profit, ratio)
+	fmt.Printf("ebt: %.2f, profit: %.2f, ratio: %.3f\n", ebt, profit, ratio)
+
+	storeResults(ebt, profit, ratio)
 }
 
-func getUserInput(infoText string) float64 {
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.2f\nProfit: %.2f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
+
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("value cannot be 0 or below")
+	}
+	return userInput, nil
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64)  {
